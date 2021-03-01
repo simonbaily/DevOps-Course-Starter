184899e3417af88179dfd049b1f38ce18d8b2f81
Simon Baily <simon.baily@capita.co.uk>
2021-03-01 16:59:26 +0000

    First working version

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,18 +1,29 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request # used for jinja and getting post requests 
 
-from todo_app.flask_config import Config
+from todo_app.flask_config import Config # not sure what this does
 
-from .data.session_items import get_items, _DEFAULT_ITEMS
+from todo_app.data.session_items import * # can use * as a wildcard to take everything from another file
+
+
 
 app = Flask(__name__)
 app.config.from_object(Config)
 
 
-@app.route('/item')
+@app.route('/', methods=['GET'])
+#@app.route('/newitem', methods=['GET', 'POST'])
 def index():
-    items = get_items()
-    return render_template('index.html', items=items)
+    items_variable = get_items()
+    return render_template('index.html', items=items_variable)
 
+
+@app.route('/newitem', methods=['POST'])
+def addNewItem():
+    newItem = request.form['createNew']
+    add_item(title = newItem)
+    items_variable = get_items()
+    return render_template('index.html', items=items_variable)
+      
 
 if __name__ == '__main__':
     app.run()
