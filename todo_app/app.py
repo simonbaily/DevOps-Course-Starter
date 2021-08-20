bf13fed92e76dc06f6ba1ac5a60f7eb3a9b75978
Simon Baily <p10002800@capita.co.uk>
2021-08-20 15:46:58 +0100

    items_variable renamed to items

--- todo_app/app.py
+++ todo_app/app.py
@@ -12,16 +12,16 @@ app.config.from_object(Config)
 #display index.html template with list of items via get_items function
 @app.route('/', methods=['GET'])
 def index():
-    items_variable = get_items()
-    return render_template('index.html', items=items_variable)
+    items = get_items()
+    return render_template('index.html', items=items)
 
 #Capture form post from /newitem form action in index.html
 @app.route('/newitem', methods=['POST'])
 def addNewItem():
     newItem = request.form['createNew']
     add_item(title = newItem)
-    items_variable = get_items()
-    return render_template('index.html', items=items_variable)
+    items = get_items()
+    return render_template('index.html', items=items)
       
 
 if __name__ == '__main__':
