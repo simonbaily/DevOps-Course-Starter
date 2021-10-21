11cb54b3cbed8c47607b412e6330df50a6b2e012
Simon Baily <p10002800@capita.co.uk>
2021-10-21 17:51:37 +0100

    Changes to add new item completed and code refactored

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,6 +1,4 @@
 from flask import Flask, render_template, redirect, request # used for jinja and getting post requests
-from todo_app.trello_config import TRELLO_URL, TRELLO_KEY, TRELLO_TOKEN, TRELLO_USERNAME, BOARD_ID_TEST #import secrets for Trello
-
 from todo_app.flask_config import Config 
 
 from todo_app.data.session_items import * # can use * as a wildcard to take everything from another file
@@ -13,14 +11,14 @@ app.config.from_object(Config)
 #display index.html template with list of items via get_items function
 @app.route('/', methods=['GET'])
 def index():
-    items = get_trello_items()
-    return render_template('index.html', items=items)
+    lists = get_trello_lists()
+    return render_template('index.html', lists=lists)
 
 #Capture form post from /newitem form action in index.html
 @app.route('/newitem', methods=['POST'])
 def addNewItem():
     newItem = request.form['createNew']
-    add_item(title = newItem)
+    add_card(name = newItem)
     return redirect('/')
       
 
